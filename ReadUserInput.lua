150cca9a5d7616a3a3ea8414fd782829bb8a76dc
UnknownDeveloqer <aavault0@gmail.com>
2024-06-06 16:18:35 -0500

    Create ReadUserInput.lua

---
+++ ReadUserInput.lua
@@ -0,0 +1,22 @@
+local input = {
+  --- Reads the user's input and returns the input.
+  ReadUserInput = function()
+      return io.read()
+  end,
+  --- Reads the user's input and returns the input in a function.
+  --- @param func fun(UserInput:string) The function to return the user's input.
+  ReadUserInput = function(func)
+      func(io.read())
+  end,
+}
+
+-- Example #1:
+print("What's your name?")
+local name = input.ReadUserInput()
+print("Hi, "..name.."!")
+
+-- Example #2
+print("What's your ID?")
+input.ReadUserInput(function(id)
+    print("So, your ID is "..id..", I assume.")
+end)
